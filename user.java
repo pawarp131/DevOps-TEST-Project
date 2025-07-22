7d01abb4b4392d871d66d06f289d1e7bf06f2818
pawarp131 <pawarprathmesh346@gmail.com>
2025-07-22 15:11:55 +0530

    JIRA101 task completed

--- user.java
+++ user.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
   /// JIRA 102
 
@@ -6,3 +7,16 @@ public class HelloWorld {
         System.out.println("Hello, My World!");
     }
 }
+=======
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git World!");
+    }
+	
+	/// JIRA 101
+	void m1(){
+		
+	}
+	
+}
+>>>>>>> Stashed changes
